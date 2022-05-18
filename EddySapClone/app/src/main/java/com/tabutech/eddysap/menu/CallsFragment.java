fe71b4f0af0501740cf2fb40d8a58d8f2e0d9f1d
Tebandeke1 <46846365+Tebandeke1@users.noreply.github.com>
2022-05-18 11:57:03 +0300

    done with status layout and calls layout with some examples as testers

--- EddySapClone/app/src/main/java/com/tabutech/eddysap/menu/CallsFragment.java
+++ EddySapClone/app/src/main/java/com/tabutech/eddysap/menu/CallsFragment.java
@@ -3,17 +3,25 @@ package com.tabutech.eddysap.menu;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.tabutech.eddysap.Adaptor.CallListAdaptor;
+import com.tabutech.eddysap.Model.CallList;
 import com.tabutech.eddysap.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CallsFragment extends Fragment {
 
-
+    private RecyclerView recyclerView;
+    List<CallList> lists = new ArrayList<>();
     public CallsFragment() {
         // Required empty public constructor
     }
@@ -29,6 +37,22 @@ public class CallsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calls, container, false);
+        View view =  inflater.inflate(R.layout.fragment_calls, container, false);
+
+        recyclerView = view.findViewById(R.id.recycleView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        getData();
+        return view;
+    }
+
+    private void getData() {
+
+        lists.add(new CallList("1","Bobi wine","23/02/2022 11:20am","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTThLlO--CKV-TsRKKt9hGdbZTRzB_xSP4YLQ&usqp=CAU","made"));
+        lists.add(new CallList("2","Spice Diana","30/05/2022 10:40pm","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsy3FoWorbAhpKDxM3OKQjwew0LwvhYvlLxg&usqp=CAU","missed"));
+        lists.add(new CallList("3","Eddy Kenzo","24/04/2022 06:34pm","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOzTRDtVlQ6A055Y8NAlTViBiDcb5_vhDDvw&usqp=CAU","received"));
+
+        recyclerView.setAdapter(new CallListAdaptor(lists,getContext()));
     }
 }
